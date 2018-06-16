cab4daa23bca017ca4089f7ee7338a01a7b35f64
Bryce Dorn <brycedorn@gmail.com>
2018-06-16 10:14:01 -0500

    rm src for gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { app } from 'hyperapp'
-import { withFx } from "@hyperapp/fx"
-
-import state from './js/state.js'
-import actions from './js/actions.js'
-import view from './js/view.js'
-
-const main = withFx(app)(state, actions, view, document.body);
